4adbd602f2da444f2c20af6308a13a4ae05fdcad
chiamablessing <chiamablessing@gmail.com>
2024-02-29 22:07:58 -0800

    Updating advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:)(.*?)(?=\])/).join(',')
-=======
-#!/usr/bin/env r#!/usr/bin/env ruby
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
->>>>>>> 76382e7ad36d7cb3046f20630b6d367c296a9b1b
